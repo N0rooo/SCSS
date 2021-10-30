207ac64b51f2f132e51f5bc93c818c346b5eeb00
“Noro” <thomas.aubert@edu.devinci.fr>
2021-10-30 15:28:42 +0200

    :white_check_mark: Site completed

--- script.js
+++ script.js
@@ -28,44 +28,67 @@ if (document.title == "Home"){
     scroll_index.addEventListener('click', scroll);
 }
 
+// Display p
 var front = document.querySelector('.front');
 var front_p = document.querySelector('.front_p');
+var front_s = document.querySelector('.s_front');
 
 var back = document.querySelector('.back');
 var back_p = document.querySelector('.back_p');
 
 var uiux = document.querySelector('.uiux');
 var uiux_p = document.querySelector('.uiux_p');
+var uiux_s = document.querySelector('.s_uiux');
 
 var others = document.querySelector('.others');
 var others_p = document.querySelector('.others_p');
 
 var soft = document.querySelector('.soft');
 var soft_p = document.querySelector('.soft_p');
+var soft_s = document.querySelector('.s_soft');
 
 if (document.title == "Skills"){
-    function displayFront(){
-        front_p.classList.toggle('visible_p');
+    function display(e){
+        e.classList.toggle('visible_p');
     }
-    function displayBack(){
-        back_p.classList.toggle('visible_p');
+    function scrolls(e){
+        window.scrollTo({
+            top:e,
+            left:0,
+            behavior: "smooth"
+        })
     }
-    function displayUiux(){
-        uiux_p.classList.toggle('visible_p');
-    }
-    function displayOthers(){
-        others_p.classList.toggle('visible_p');
-    }
-    function displaySoft(){
-        soft_p.classList.toggle('visible_p');
-    }
+    front.addEventListener('click', () => display(front_p));
+    back.addEventListener('click', () => display(back_p));
+    uiux.addEventListener('click', () => display(uiux_p));
+    others.addEventListener('click', () => display(others_p));
+    soft.addEventListener('click', () => display(soft_p));  
     
-    
-    front.addEventListener('click', displayFront);
-    back.addEventListener('click', displayBack);
-    uiux.addEventListener('click', displayUiux);
-    others.addEventListener('click', displayOthers);
-    soft.addEventListener('click', displaySoft);
 
+    
+    front_s.addEventListener('click', () => scrolls(300));
+    uiux_s.addEventListener('click', () => scrolls(850));
+    soft_s.addEventListener('click', () => scrolls(1570));
 }
 
+// Summary
+var mobile = document.querySelector('.mobile');
+var teacher = document.querySelector('.teacher');
+var pulv = document.querySelector('.pulv');
+var website = document.querySelector('.website');
+var kk = document.querySelector('.kk');
+
+if (document.title == "Projects"){
+    function scroll(e){
+        window.scrollTo({
+            top:e,
+            left:0,
+            behavior: "smooth"
+        })
+    }
+    mobile.addEventListener('click', () => scroll(300));
+    teacher.addEventListener('click', () => scroll(400));
+    pulv.addEventListener('click', () => scroll(1570));
+    website.addEventListener('click', () => scroll(4980));
+    kk.addEventListener('click', () => scroll(5100));
+}
